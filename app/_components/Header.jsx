2b917446c7d132ed6b1bf65ca0e862f24b1042eb
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-10-08 15:40:09 +0200

    first commit

---
+++ app/_components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+function Header() {
+  return (
+    <div className='flex justify-between'>
+      <Image
+      src={'/logo.webp'}
+      width={100}
+      height={100}
+      />
+      <Button>Beginne jetzt</Button>
+    </div>
+  )
+}
+
+export default Header
---
+++ app/_components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='flex justify-center'>
+      Text Hero
+    </div>
+  )
+}
+
+export default Hero
