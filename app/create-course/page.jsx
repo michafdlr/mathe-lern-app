3d706c9f7d35b83853a4140cbdbda2b764396ae7
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-10-12 12:37:10 +0200

    create-course page changes

--- app/create-course/page.jsx
+++ app/create-course/page.jsx
@@ -27,21 +27,21 @@ function CreateCourse() {
   return (
     <div className='flex flex-col justify-center items-center mt-8'>
       <h1 className='md:text-4xl text-2xl text-secondary'>
-        Erstellen Sie einen neuen Lernpfad
+        Erstelle einen neuen Lernpfad
       </h1>
 
       {/* Stepper */}
       <div className='flex mt-10'>
         {stepperOptions.map((item, index) => {
           return (
-            <div className='flex items-center'>
+            <div className='flex items-center' key={index}>
               <div className='flex flex-col items-center mx-4 w-[50px] md:w-[100px]'>
                 <div className={`bg-gray-400 p-3 rounded-full text-white ${activeIndex>=index && 'bg-[#26b5c7]'}`}>
                   {item.icon}
                 </div>
                 <h2 className='hidden md:text-sm md:block'>{item.name}</h2>
               </div>
-              {index != stepperOptions?.length-1 && <div className='h-1 w-[50px] md:w-[100px] rounded-full lg:w-[170px] bg-gray-400'></div>}
+              {index != stepperOptions?.length-1 && <div className={`h-1 w-[50px] md:w-[100px] rounded-full lg:w-[170px] bg-gray-400 ${activeIndex>=index+1 && 'bg-[#26b5c7]'}`}></div>}
             </div>
           )
         })}
@@ -52,10 +52,13 @@ function CreateCourse() {
       {/* Next and Previous Button */}
       <div className='flex justify-between items-center'>
         <Button onClick={() => {
-          setActiveIndex(activeIndex+1);
-          console.log(activeIndex)
-          }}>Weiter</Button>
-        <Button>Zurück</Button>
+          setActiveIndex(activeIndex+1)
+          }}>
+            Weiter
+        </Button>
+        <Button onClick={() => setActiveIndex(activeIndex-1)}>
+          Zurück
+        </Button>
       </div>
     </div>
   )
