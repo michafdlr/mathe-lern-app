3f44ee2e1c958cdc3d87192429cd97ed21cd84ee
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-10-29 19:16:05 +0100

    Dashboard updates

--- app/dashboard/_components/CourseCard.jsx
+++ app/dashboard/_components/CourseCard.jsx
@@ -1,18 +1,34 @@
 import Image from 'next/image'
 import React from 'react'
+import { HiBookOpen, HiChartBar } from "react-icons/hi2";
 
 function CourseCard({ course }) {
   return (
-    <div className='border-2 rounded-lg p-3 m-2'>
+    <div className='border-2 rounded-lg p-2 shadow-sm hover:scale-105 transition-all duration-200 cursor-pointer'>
       <Image
       src={course?.courseBanner || "/placeholder.png"} //5:00:52
-      width={200}
-      height={200}
-      style={{width: 'auto', height: 'auto'}}
+      width={300}
+      height={150}
+      style={{width: 'auto', height: '150px'}}
       alt="Kursbild"
       priority={true}
+      className='w-full h-[250px] object-scale-down'
       />
-      {course?.theme}
+      <div className='p-2'>
+        <h2 className='font-bold text-lg flex-none'>{course?.theme}</h2>
+
+        <div className='flex justify-between mt-2'>
+          <h2 className='flex gap-2 items-center p-1 bg-cyan-100 rounded-md text-sm'>
+            <HiBookOpen />
+            {course?.courseOutput?.kapitel?.length} Kapitel
+          </h2>
+
+          <h2 className='flex gap-2 items-center p-1 bg-cyan-100 rounded-md text-sm'>
+            <HiChartBar className={`${course?.difficulty == 'leicht' ? 'text-green-500' : course?.difficulty == 'mittel' ? 'text-yellow-500': 'text-red-500'}`}/>
+            {course?.difficulty}
+          </h2>
+        </div>
+      </div>
     </div>
   )
 }
--- app/dashboard/_components/UserCourseList.jsx
+++ app/dashboard/_components/UserCourseList.jsx
@@ -5,6 +5,7 @@ import { useUser } from '@clerk/nextjs'
 import { eq } from 'drizzle-orm'
 import React, { useEffect, useState } from 'react'
 import CourseCard from './CourseCard'
+import SubjectList from '@/app/_shared/SubjectList'
 
 function UserCourseList() {
   const [courseList, setCourseList] = useState([]);
@@ -22,12 +23,29 @@ function UserCourseList() {
     <div className='mt-2 mx-2'>
       <h2 className='text-secondary text-lg md:text-xl font-bold'>
         Meine Lernpfade
-    </h2>
-      {courseList?.map((course, index) => {
-        return (
-          <CourseCard key={index} course={course}/>
-        )
-      })}
+      </h2>
+      <div>
+        {
+          SubjectList?.map((subject, idx) => {
+            return (
+              <div key={idx} className='flex flex-col my-2'>
+                <h2 className='text-md md:text-lg font-bold underline text-primary'>
+                  {subject.name}
+                </h2>
+                  <div className='mt-2 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-2'>
+                    {courseList?.map((course, index) => {
+                      if (course?.subject == subject.name) {
+                        return (
+                          <CourseCard key={index} course={course}/>
+                        )
+                      }
+                    })}
+                  </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
